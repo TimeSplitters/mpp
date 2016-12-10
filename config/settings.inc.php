d652c15090de638632f96f065ec5e187a4d1d56e
Christophe Abillama <christophe@nvgallery.com>
2016-12-10 11:45:05 +0100

    config

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,8 +1,16 @@
 <?php
-define('_DB_SERVER_', 'localhost');
-define('_DB_NAME_', 'potpourri');
-define('_DB_USER_', 'root');
-define('_DB_PASSWD_', 'root');
+if($_SERVER['SERVER_NAME'] == 'www.monpotpourri.com') {
+    define('_DB_SERVER_', 'monpotposi74.mysql.db');
+    define('_DB_NAME_', 'monpotposi74');
+    define('_DB_USER_', 'monpotposi74');
+    define('_DB_PASSWD_', 'rq0uVEZZRCLL');
+} else {
+    define('_DB_SERVER_', 'localhost');
+    define('_DB_NAME_', 'potpourri');
+    define('_DB_USER_', 'root');
+    define('_DB_PASSWD_', 'root');
+}
+
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheFs');
